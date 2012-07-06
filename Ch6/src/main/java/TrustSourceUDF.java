d16831a2af86935e603ad2cc0c2fd3deeb6eee9b
Brian,Femiano <bfemiano@gmail.com>
2012-07-06 19:02:44 -0400

    finished my portion of code for Ch6

---
+++ Ch6/src/main/java/TrustSourceUDF.java
@@ -0,0 +1,31 @@
+import org.apache.hadoop.hive.ql.exec.UDF;
+import org.apache.hadoop.io.Text;
+
+import java.lang.String;import java.util.HashSet;
+import java.util.Set;
+
+
+public class TrustSourceUDF extends UDF {
+
+    private static Set<String> untrustworthySources = new HashSet<String>();
+    private Text result = new Text();
+
+    static{
+        untrustworthySources.add("");
+        untrustworthySources.add("\"\"\"http://www.afriquenligne.fr/3-soldiers\"");
+        untrustworthySources.add("Africa News Service");
+        untrustworthySources.add("Asharq Alawsat");
+        untrustworthySources.add("News Agency of Nigeria (NAN)");
+        untrustworthySources.add("This Day (Nigeria)");
+    }
+
+    public Text evaluate(Text source) {
+
+         if (untrustworthySources.contains(source.toString())){
+             result.set("no");
+         } else {
+             result.set("yes");
+         }
+         return result;
+    }
+}
